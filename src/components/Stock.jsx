57f1da70d9682d77de100514da6886778bdefd21
Piotr Florczak <piotrflorczakk@gmail.com>
2019-06-08 16:29:25 +0200

    Dodanie komponentu Stock

---
+++ src/components/Stock.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Stock(props) {
+  console.log(props);
+  const elements = props.stock.map(item => {
+    return (
+      <div key={item.id} className="border stock">
+        <div>
+          <h4>{item.shortname}</h4>
+          <h3>{item.name}</h3>
+        </div>
+        <h3 className="non-bold">{`${item.value} kg`}</h3>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h4>Stan magazynu</h4>
+      <div className="two-columns">{elements}</div>
+      <hr />
+    </div>
+  );
+}
+
+export default Stock;
--- src/components/Workplace.jsx
+++ src/components/Workplace.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import Order from "./Order";
+import Stock from "./Stock";
 
 class Workplace extends Component {
   constructor() {
     super();
     this.state = {
       id: 1,
-      workplaceName: "Skelp 1"
+      workplaceName: "Skelp 1",
+      stock: [
+        {
+          id: 1,
+          name: "Złom stalowy",
+          shortname: "St",
+          value: "2 231,202"
+        },
+        {
+          id: 2,
+          name: "Metal kolorowy",
+          shortname: "Kl",
+          value: "231,120"
+        }
+      ]
     };
   }
   render() {
-    return <Order workplaceName={this.state.workplaceName} />;
+    return (
+      <div>
+        <Order workplaceName={this.state.workplaceName} />
+        <div className="two-columns">
+          <Stock stock={this.state.stock} />
+        </div>
+      </div>
+    );
   }
 }
 
