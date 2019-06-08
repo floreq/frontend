56e50f918a9448a20858430a091b2fa1a623f85d
Piotr Florczak <piotrflorczakk@gmail.com>
2019-06-08 15:02:11 +0200

    Dodanie komponentu order

---
+++ src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Form(props) {
+  console.log(props);
+  return (
+    <form>
+      <label>
+        <h4>W dniu</h4>
+        <input
+          type="text"
+          name="date"
+          value={`${props.date.day}-${props.date.month}-${props.date.year}`}
+          onChange={props.handleChange}
+        />
+      </label>
+      <label>
+        <h4>Zadanie</h4>
+        <select>
+          <option value="volvo">Zakup</option>
+          <option value="saab">Odbiór</option>
+          <option value="mercedes">Zaliczka</option>
+          <option value="audi">Wpływy</option>
+          <option value="audi">Wydatki</option>
+        </select>
+      </label>
+      <label>
+        <h4>Komentarz</h4>
+        <input type="text" />
+      </label>
+      <label>
+        <h4>Kwota</h4>
+        <input type="text" />
+        <span>zł</span>
+      </label>
+      <label>
+        <h4>Ilość (w kg)</h4>
+        <input type="text" />
+      </label>
+      <label>
+        <h4>Rodzaj złomu</h4>
+        <select>
+          <option value="stalowy">Stalowy</option>
+          <option value="kolorowy">Kolorowy</option>
+        </select>
+      </label>
+      <button>Zatwierdź</button>
+    </form>
+  );
+}
+
+export default Form;
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,44 +1,34 @@
 import React, { Component } from "react";
+import Form from "./Form";
 
 class Order extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      date: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    console.log(event.target.name);
+  }
+
   render() {
+    const myDate = this.state.date;
+    const day =
+      myDate.getDate() < 10 ? "0" + myDate.getDate() : myDate.getDate();
+    const month =
+      myDate.getMonth() + 1 < 10
+        ? "0" + (myDate.getMonth() + 1)
+        : myDate.getMonth() + 1;
+    const year = this.state.date.getFullYear();
     return (
       <div>
         <h4>Zlecenie</h4>
         <h1>{this.props.workplaceName}</h1>
-        <form>
-          <label>
-            <h4>W dniu</h4>
-            <input type="text" />
-          </label>
-          <label>
-            <h4>Zadanie</h4>
-            <select>
-              <option value="volvo">Zakup</option>
-              <option value="saab">Odbiór</option>
-              <option value="mercedes">Zaliczka</option>
-              <option value="audi">Wpływy</option>
-              <option value="audi">Wydatki</option>
-            </select>
-          </label>
-          <label>
-            <h4>Kwota</h4>
-            <input type="text" />
-          </label>
-          <label>
-            <h4>Ilość (w kg)</h4>
-            <input type="text" />
-          </label>
-          <label>
-            <h4>Rodzaj materiału</h4>
-            <input type="text" />
-          </label>
-          <button>Zatwierdź</button>
-        </form>
+        <Form date={{ day, month, year }} handleChange={this.handleChange} />
+        <hr />
       </div>
     );
   }
