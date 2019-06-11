cf0fe9396e2d0361d3c20246e5a7df8ebe7ac54a
Piotr Florczak <piotrflorczakk@gmail.com>
2019-06-11 20:10:24 +0200

    Walidacja formularza

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 
 function Form(props) {
-  // Czy powinien zostac wyswietlony extendedTask (rozszerzony formularz)
-  const showAll = props.ifExtendedTask.includes(props.enteredTask.task);
-
+  const fError = props.formErrors;
   // Przypisanie do zmiennych pol zaleznych od wyboru
   let quantity = (
     <label>
       <h4>Ilość</h4>
       <input
-        className="align-right"
+        className={
+          fError.quantity.length > 0 ? "error align-right" : "align-right"
+        }
         type="text"
         name="quantity"
-        value={showAll ? props.enteredTask.quantity : ""}
+        value={props.enteredTask.quantity}
         onChange={props.handleChange}
       />
       <span>kg</span>
+      {fError.quantity.length > 0 && (
+        <span className="error-message">{fError.quantity}</span>
+      )}
     </label>
   );
   let type = (
     <label>
       <h4>Rodzaj złomu</h4>
       <select
+        className={props.formErrors.type.length > 0 ? "error" : null}
         name="type"
         // Jesli sie nie zmienilo opcji, wartosc zostawala pusta
         value={
@@ -34,23 +38,31 @@ function Form(props) {
         <option value="St">Stalowy</option>
         <option value="Kl">Kolorowy</option>
       </select>
+      {fError.type.length > 0 && (
+        <span className="error-message">{fError.type}</span>
+      )}
     </label>
   );
 
   return (
-    <form onSubmit={props.handleSubmit}>
+    <form onSubmit={props.handleSubmit} noValidate>
       <label>
         <h4>W dniu</h4>
         <input
+          className={fError.dateOfEntry.length > 0 ? "error" : null}
           type="text"
           name="dateOfEntry"
           value={props.enteredTask.dateOfEntry}
           onChange={props.handleChange}
         />
+        {fError.dateOfEntry.length > 0 && (
+          <span className="error-message">{fError.dateOfEntry}</span>
+        )}
       </label>
       <label>
         <h4>Zadanie</h4>
         <select
+          className={fError.task.length > 0 ? "error" : null}
           name="task"
           // Jesli sie nie zmienilo opcji, wartosc zostawala pusta
           value={
@@ -66,6 +78,9 @@ function Form(props) {
           <option value="wplywy">Wpływy</option>
           <option value="wydatki">Wydatki</option>
         </select>
+        {fError.task.length > 0 && (
+          <span className="error-message">{fError.task}</span>
+        )}
       </label>
       <label>
         <h4>Komentarz</h4>
@@ -79,16 +94,23 @@ function Form(props) {
       <label>
         <h4>Kwota</h4>
         <input
-          className="align-right"
+          className={
+            props.formErrors.expense.length > 0
+              ? "align-right error"
+              : "align-right"
+          }
           type="text"
           name="expense"
           value={props.enteredTask.expense}
           onChange={props.handleChange}
         />
         <span>zł</span>
+        {fError.expense.length > 0 && (
+          <span className="error-message">{fError.expense}</span>
+        )}
       </label>
-      {showAll ? quantity : null}
-      {showAll ? type : null}
+      {quantity}
+      {type}
       <button>Zatwierdź</button>
     </form>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
+import { isValidDate } from "./../scripts/isValidDate";
 import Form from "./Form";
+
+const formValid = ({ formErrors, enteredTask }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(enteredTask).forEach(val => {
+    val === "" && (valid = false);
+  });
+
+  return valid;
+};
 
 class Task extends Component {
   constructor() {
     super();
     this.state = {
-      // extendedTask wyznacza wartosci kiedy powinien zostac wyswietlony caly formularz
-      extendedTask: ["zakup", "odbior"],
+      // enteredTask przechowuje wprowadzane dane przez uzytkownika
       enteredTask: {
+        dateOfEntry: "",
+        task: "",
+        comment: "",
+        expense: "",
+        quantity: "",
+        type: ""
+      },
+      possibleTasks: ["zakup", "odbior", "zaliczka", "wplywy", "wydatki"],
+      possibleTypes: ["St", "Kl"],
+      // formErrors przechowuje bledy walidacyjne (np. Niewlasciwy format daty)
+      formErrors: {
         dateOfEntry: "",
         task: "",
         comment: "",
@@ -34,25 +61,73 @@ class Task extends Component {
     this.setState(prevState => ({
       enteredTask: {
         ...prevState.enteredTask,
-        dateOfEntry: `${day}-${m}-${year}`
+        dateOfEntry: `${day}.${m}.${year}`
       }
     }));
   }
 
   handleChange(event) {
     const { name, value } = event.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "dateOfEntry":
+        formErrors.dateOfEntry = isValidDate(value) ? "" : "dd.mm.rrrr";
+        break;
+      // case "comment":
+      //   formErrors.comment =
+      //     value.length < 3 ? "minimum 3 characaters required" : "";
+      //   break;
+      case "task":
+        formErrors.task = this.state.possibleTasks.includes(value)
+          ? ""
+          : "Brak takiego zadania";
+        break;
+      case "expense":
+        formErrors.expense = isNaN(value) ? "np. 10.99" : "";
+        break;
+      case "quantity":
+        formErrors.quantity = isNaN(value) ? "np. 1100.90" : "";
+        break;
+      case "type":
+        formErrors.type = this.state.possibleTypes.includes(value)
+          ? ""
+          : "Brak takiego rodzaju";
+        break;
+      default:
+        break;
+    }
 
     this.setState(prevState => ({
       enteredTask: {
         ...prevState.enteredTask,
         [name]: value
-      }
+      },
+      formErrors
     }));
+    console.log(this.state);
   }
 
   // Dodanie zadania do listy wszystkich zadan
   handleSubmit(event) {
     event.preventDefault();
+
+    if (formValid(this.state)) {
+      this.setState(prevState => {
+        const newTaskEntry = prevState.enteredTask;
+        let tasksList = prevState.tasks;
+        tasksList.unshift(newTaskEntry);
+        return {
+          tasks: tasksList
+        };
+      });
+
+      this.props.handleTransfer(this.state.tasks);
+
+      console.log("Submited", this.state);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
     this.setState(prevState => {
       const newTaskEntry = prevState.enteredTask;
       let tasksList = prevState.tasks;
@@ -61,8 +136,6 @@ class Task extends Component {
         tasks: tasksList
       };
     });
-
-    this.props.handleTransfer(this.state.tasks);
   }
 
   render() {
@@ -71,7 +144,7 @@ class Task extends Component {
         <h4>Zlecenie</h4>
         <h1>{this.props.workplaceName}</h1>
         <Form
-          ifExtendedTask={this.state.extendedTask}
+          formErrors={this.state.formErrors}
           enteredTask={this.state.enteredTask}
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
